ca3fbf0bee24cde1e3b4c33fd05db0a028a5b75b
Luke100000 <jonas.peche@aon.at>
2025-02-25 22:13:39 +0100

    name conflict

--- common/src/main/java/immersive_machinery/Messages.java
+++ common/src/main/java/immersive_machinery/Messages.java
@@ -7,8 +7,8 @@ import immersive_machinery.network.c2s.TunnelDiggerControlsUpdate;
 
 public class Messages {
     public static void loadMessages() {
-        NetworkHandler.registerMessage(Common.SHORT_MOD_ID, TunnelDiggerControlsUpdate.class, TunnelDiggerControlsUpdate::new);
-        NetworkHandler.registerMessage(Common.SHORT_MOD_ID, BambooBeeConfigurationUpdate.class, BambooBeeConfigurationUpdate::new);
-        NetworkHandler.registerMessage(Common.SHORT_MOD_ID, SonarMessage.class, SonarMessage::new);
+        NetworkHandler.registerMessage(Common.MOD_ID, TunnelDiggerControlsUpdate.class, TunnelDiggerControlsUpdate::new);
+        NetworkHandler.registerMessage(Common.MOD_ID, BambooBeeConfigurationUpdate.class, BambooBeeConfigurationUpdate::new);
+        NetworkHandler.registerMessage(Common.MOD_ID, SonarMessage.class, SonarMessage::new);
     }
 }
